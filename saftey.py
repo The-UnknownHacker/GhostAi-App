a1ec26261b91b63b145f6962bbf59283e292c42d
theunknownhacker <128781393+The-UnknownHacker@users.noreply.github.com>
2024-01-20 16:38:34 +1100

    Add a function to saftey.py

--- saftey.py
+++ saftey.py
@@ -1,1 +1,23 @@
+# Add the saftey fuction module to a different file
+
+from google.generativeai.types.safety_types import HarmBlockThreshold
+
+safety_settings = [
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": HarmBlockThreshold.BLOCK_NONE,
+    },
+]
 
